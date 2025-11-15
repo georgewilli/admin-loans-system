634278bd6176e50a8165b2beea2ea7bc83f6f7b9
george.william <george.wiliam@callvita.com>
2025-11-15 21:00:36 +0200

    init backend

---
+++ backend/prisma.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, env } from "prisma/config";
+import * as dotenv from "dotenv";
+
+dotenv.config();  // <--- ADD THIS LINE
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
